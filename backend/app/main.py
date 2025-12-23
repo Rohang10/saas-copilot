057196e6f70617f80de36154a1221aa9fa9eacd1
Rohang Shah <rohangshah1003@gmail.com>
2025-12-23 21:19:29 +0530

    done

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,7 +32,7 @@ app.middleware("http")(log_requests)
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=os.getenv("CORS_ORIGINS", "").split(","),
-    allow_origins=["http://localhost:5173",  "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173",  "http://127.0.0.1:5173", "https://saas-copilot-ten.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
